b8c4727a96583e84570356d10c8b8cf4858f125e
Ernesto <epetrich9@gmail.com>
2025-10-09 17:50:41 -0300

    empezamos implementacion DB

---
+++ trabajos/Trabajos Prácticos Grupales/Trabajo Practico 6/back/src/common/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+SQLITE_DATABASE_URL = "sqlite:///./ecoHarmonyDB.db"
+
+engine = create_engine(
+    SQLITE_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+
+    finally:
+        db.close()
+        
